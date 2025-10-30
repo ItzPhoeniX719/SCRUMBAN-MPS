e50084d9691ba74a4523ada598a3e849064d54a5
alexdrin <162601458+alexdrin@users.noreply.github.com>
2025-10-30 11:28:07 +0100

    Add files via upload

--- matrix.c
+++ matrix.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include "IOMATRIX.h"
+#include "IOMATRIX.h"
 #include "OPMATRIX.h"
 
 Matriz suma(Matriz A, Matriz B);
 Matriz resta(Matriz A, Matriz B);
 Matriz productoEscalar(int escalar, Matriz A);
 Matriz producto(Matriz A, Matriz B);
+Matriz matrixLoad(const char* filename);
+void matrixSave(const Matriz m, const char* filename);
+void matrixPrint(const Matriz m);
 
 #define N 5
 
 int main(){
-    int opcion, k, i, j;
+    int opcion, k, i, j, modo;
     Matriz A, B, R;
+    char ficheroA[100], ficheroB[100];
     //char logFile[] = "logs/matrix_log.txt";
 
     printf("===========================================\n");
@@ -33,65 +37,78 @@ int main(){
         printf("5. Salir\n");
         scanf("%d", &opcion);
 
-        if(opcion >= 1 && opcion <= 3){
-            printf("Introduce los elementos de la matriz A:\n");
-            for(i = 0; i < N; i++){
-                for(j = 0; j < N; j++){
-                    printf("A[%d][%d]: ", i, j);
-                    scanf("%d", &A.mat[i][j]);
+        if(opcion >= 1 && opcion <= 4){
+            printf("\nSelecciona el modo de entrada de las matrices:\n");
+            printf("1. Introducir manualmente\n");
+            printf("2. Cargar desde ficheros\n");
+            printf("Opción: ");
+            scanf("%d", &modo);
+
+            if(modo == 1){//Carga manual
+                printf("\nIntroduce los elementos de la matriz A:\n");
+                int i = 0, j = 0;
+                for (i = 0; i < 5; i++)
+                    for (j = 0; j < 5; j++)
+                    {
+                        printf("A[%d][%d]: ", i, j);
+                        scanf("%d", &A.mat[i][j]);
+                    }
+
+                if (opcion != 4){
+                printf("\nIntroduce los valores de la matriz B:\n");
+                int i = 0, j = 0;
+                for (i = 0; i < 5; i++)
+                    for ( j = 0; j < 5; j++)
+                    {
+                        printf("B[%d][%d]: ", i, j);
+                        scanf("%d", &B.mat[i][j]);
+                    }
                 }
+            }else if(modo == 2){
+                // --- Entrada desde fichero ---
+                printf("\nIntroduce el nombre del fichero de la matriz A: ");
+                scanf("%s", ficheroA);
+                A = matrixLoad(ficheroA);  
+
+            if (opcion != 4)
+            {
+                printf("Introduce el nombre del fichero de la matriz B: ");
+                scanf("%s", ficheroB);
+                B = matrixLoad(ficheroB);
+            }
+            }
+            else
+            {
+                printf("Opción de modo no válida. Volviendo al menú principal...\n");
+                continue;
             }
 
-            printf("Matriz A ingresada:\n");
-            printf("introduce los elementos de la matriz B:\n");
-            for(i = 0; i < N; i++){
-                for(j = 0; j < N; j++){
-                    printf("B[%d][%d]: ", i, j);
-                    scanf("%d", &B.mat[i][j]);
-                }
-            }
         }
-
-        if(opcion == 4){
-            printf("Introduce los elementos de la matriz A:\n");
-            for(i = 0; i < N; i++){
-                for(j = 0; j < N; j++){
-                    printf("A[%d][%d]: ", i, j);
-                    scanf("%d", &A.mat[i][j]);
-                }
-            }
-            printf("Introduce el escalar k: ");
-            scanf("%d", &k);
-        }
-
-        int i = 0; int j = 0;
-        for(i; i<5; i++){
-            for(j; j<5; j++){
-                printf("%d ", R.mat[i][j]);
-            }
-            printf("\n");
-        }
+    
 
         switch (opcion){
             case 1:
                 R = suma(A, B);
-                printf("Suma de matrices seleccionada.\n");
+                printf("\n--- Resultado de la suma ---\n");
+                matrixPrint(R);
                 break;
             case 2:
                 R = resta(A, B);
-                printf("Resta de matrices seleccionada.\n");
+                printf("\n--- Resultado de la resta ---\n");
+                matrixPrint(R);
                 break;
             case 3:
-                R = productoEscalar(k, A);
-                printf("Producto escalar seleccionado.\n");
+                R = producto(A, B);
+                printf("\n--- Resultado de la multiplicacion ---\n");
+                matrixPrint(R);
                 break;
             case 4:
-                R = producto(A, B);
-                printf("Multiplicacion de matrices seleccionada.\n");
+                R = productoEscalar(k, A);
+                printf("\n--- Resultado del producto escalar ---\n");
+                matrixPrint(R);
                 break;
         }
     } while (opcion != 5);
     
     return 0;
-
 }
